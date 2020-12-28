67025e4542f18a403f5d70a3b98d158b239e5495
cdsalty <soltiscd@gmail.com>
2020-12-28 17:25:53 -0500

    graphQL backend setup complete

--- schema.js
+++ schema.js
@@ -2,14 +2,14 @@ const axios = require("axios");
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt,
   GraphQLList,
+  GraphQLInt,
   GraphQLSchema
 } = require("graphql");
+const moment = require("moment");
 
 const TeamType = new GraphQLObjectType({
-  name: "Team", // name of the object getting returned
-  // fields matches the data coming back from the api
+  name: "Team",
   fields: () => ({
     id: { type: GraphQLInt },
     city: { type: GraphQLString },
@@ -24,11 +24,10 @@ const TeamType = new GraphQLObjectType({
 const GameType = new GraphQLObjectType({
   name: "Game",
   fields: () => ({
-    id: { type: GrpahQLInt },
+    id: { type: GraphQLInt },
     date: { type: GraphQLString },
-    home_team_score: { type: GrpahQLInt },
-    visitor_team_score: { type: GrpahQLInt },
-    // the home_team data coming back is the same layout as "TeamType" object
+    home_team_score: { type: GraphQLInt },
+    visitor_team_score: { type: GraphQLInt },
     home_team: { type: TeamType },
     visitor_team: { type: TeamType },
     status: { type: GraphQLString }
@@ -52,13 +51,14 @@ const RootQuery = new GraphQLObjectType({
           console.log(data.data);
           return data.data;
         } catch (error) {
-          console.log(error);
+          console.error(error);
         }
       }
     },
     // get Specific Team (give an id and it should return specific information about that team)
     team: {
       type: TeamType,
+      // make use of ARGS in order to get specific info
       args: {
         id: { type: GraphQLInt }
       },
@@ -67,10 +67,51 @@ const RootQuery = new GraphQLObjectType({
           const response = await axios.get(
             `https://www.balldontlie.io/api/v1/teams/${args.id}`
           );
-          const data = await response;
-          return data.data;
+          const { data } = await response;
+          // console.log(data.data);
+          return data;
         } catch (error) {
-          console.log(error);
+          console.error(error);
+        }
+      }
+    },
+    // GAME(s) info (refer back to GameType from above)
+    allGames: {
+      type: new GraphQLList(GameType),
+      // ARGS is the way you want to specifically define the type/way you make the search in qraphIql
+      args: { date: { type: GraphQLString } },
+      async resolve(parents, args) {
+        try {
+          const res = await axios.get(
+            `https://www.balldontlie.io/api/v1/games/?start_date=${args.date}&end_date=${args.date}`
+          );
+          const { data } = await res;
+          return data.data;
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    },
+    teamGames: {
+      type: new GraphQLList(GameType),
+      args: {
+        id: { type: GraphQLInt }
+      },
+      async resolve(parent, args) {
+        try {
+          const response = await axios.get(
+            `https://www.balldontlie.io/api/v1/games?teams_ids[]=${
+              args.id
+            }&start_date=${moment()
+              .subtract(7, "days")
+              .format("YYYY-MM-DD")}&end_date=${moment()
+              .add(7, "days")
+              .format("YYYY-MM-DD")}`
+          );
+          const data = await response;
+          return data.data.data;
+        } catch (error) {
+          console.error(error);
         }
       }
     }
@@ -90,4 +131,39 @@ Team Query:
     id
   }
 }
+
+FOR ALLGAMES: ON A SPECIFIC DAY 
+{
+  allGames(date: "2020-12-25") {
+    date
+    home_team {
+      id
+      name
+      city
+    }
+    home_team_score
+    visitor_team {
+      id
+      name
+      city
+    }
+    visitor_team_score
+  }
+}
+
+{
+  teamGames(id: 5) {
+    home_team_score
+    visitor_team_score
+    home_team {
+      city
+      name
+    }
+    visitor_team {
+      city
+      name
+    }
+  }
+}
+cmd + "spacebar": inside graphIql, this will show you all the types you can search... 
 */
