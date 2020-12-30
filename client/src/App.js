926c1fe80b7d9123db6ee05e6b4c7f9fed962e61
cdsalty <soltiscd@gmail.com>
2020-12-30 16:39:04 -0500

    setup for graphql connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+  from
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import "./App.css";
 
-function App() {
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+// Suggested wayt to start buiding your API for error handling?
+const link = from([
+  errorLink,
+  new HttpLlink({ uri: "http://localhost:5000/graphql" })
+]);
+
+// to create connection
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+});
+
+const App = () => {
   return (
     <div className="App">
       <h1>Working</h1>
+      <h4>Ready to setup Apollo Boiler Plate</h4>
     </div>
   );
-}
+};
 
 export default App;
